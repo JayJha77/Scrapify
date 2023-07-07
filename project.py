51dbafcfb7d77f90e6b706eb398f6837e8216a0e
Jay <jhajay438@gmail.com>
2023-07-08 00:41:06 +0530

    some error left

--- project.py
+++ project.py
@@ -3,9 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 
 Product_name = []
-Price = []
+price = []
 Description = []
 reviews = []
+#for i in range(2,10):
 
 url = "https://www.flipkart.com/search?q=best+mobile+under+20000rs&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_2_28_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_2_28_na_na_na&as-pos=2&as-type=RECENT&suggestionId=best+mobile+under+20000rs%7CMobiles&requestId=cbfff848-89eb-47ff-8619-0cf1ccbe75e2&as-searchtext=best+mobile+phone+under+2000&page="+str(1)
 
@@ -26,9 +27,9 @@ for i in name:
 prices = box.find_all("div",class_="_30jeq3 _1_WHN1")
 
 
-for i in prices:
+for i in price:
     name = i.text
-    Price.append(name)
+    price.append(name)
 #print(Price)
 
 desc = box.find_all("ul",class_="_1xgFaf")
@@ -47,10 +48,10 @@ for i in Reviews:
     reviews.append(name)
 #print(reviews)
 
-df = pd.DataFrame({"Product_name":Product_name,"prices":prices,"Description":"Description","reviews":"reviews"})
+df = pd.DataFrame({"Product_name":Product_name,"prices":prices,"Description":Description,"reviews":reviews})
 print(df)
 
-#df.to_csv("C:/Users/jayjha/Desktop/git/flipcard_mobile_datset.csv")
+df.to_csv("C:/Users/jayjha/Desktop/git/flipcard_mobile_datset.csv")
 
 
    
